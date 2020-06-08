7641a3ce3938cf41741b283a343768b3aa9948d2
Timothe Lebobe <timothe.lebobe@gmail.com>
2020-06-08 23:05:15 +0200

    premier test de mainpulation d'image

---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# le \ permet de continuer la ligne précédentes à la ligne suivantes
+# les il signifie le programme
+
+import PIL
+
+image = PIL.Image("13-photo.jpg")
+image.show()
